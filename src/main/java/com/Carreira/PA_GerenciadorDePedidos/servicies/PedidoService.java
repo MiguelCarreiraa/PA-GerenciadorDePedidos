3ebf94e5480d1859a6cfda2be3173dc91428af6e
MiguelCarreiraa <miguelitocarreira@hotmail.com>
2026-03-18 16:34:44 -0300

    Pedido Service

---
+++ src/main/java/com/Carreira/PA_GerenciadorDePedidos/servicies/PedidoService.java
@@ -0,0 +1,40 @@
+package com.Carreira.PA_GerenciadorDePedidos.servicies;
+
+import com.Carreira.PA_GerenciadorDePedidos.models.PedidoModel;
+import com.Carreira.PA_GerenciadorDePedidos.repositories.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PedidoService {
+
+    @Autowired
+        private PedidoRepository pedidoRepository;
+
+    public List<PedidoModel> findAll(){
+        return pedidoRepository.findAll();
+    }
+
+    public PedidoModel criarPedido(PedidoModel pedidoModel){
+        return pedidoRepository.save(pedidoModel);
+    }
+
+    public Optional<PedidoModel> buscarPorId(Long id){
+        return pedidoRepository.findById(id);
+    }
+
+    public PedidoModel atualizar(Long id, PedidoModel pedidoModel){
+        PedidoModel model = pedidoRepository.findById(id).get();
+        model.setData(pedidoModel.getData());
+        model.setValorTotal(pedidoModel.getValorTotal());
+        model.setStatus(pedidoModel.getStatus());
+        return pedidoRepository.save(model);
+    }
+
+    public void deletar(Long id){
+        pedidoRepository.deleteById(id);
+    }
+}

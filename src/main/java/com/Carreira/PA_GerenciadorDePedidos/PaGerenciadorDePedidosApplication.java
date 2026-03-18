af423649f923c81da73f91e3bdd0ff5e7689e9ad
MiguelCarreiraa <miguelitocarreira@hotmail.com>
2026-03-18 16:21:29 -0300

    Pedido Model

---
+++ src/main/java/com/Carreira/PA_GerenciadorDePedidos/PaGerenciadorDePedidosApplication.java
@@ -0,0 +1,13 @@
+package com.Carreira.PA_GerenciadorDePedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaGerenciadorDePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaGerenciadorDePedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Carreira/PA_GerenciadorDePedidos/PaGerenciadorDePedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Carreira.PA_GerenciadorDePedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaGerenciadorDePedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
